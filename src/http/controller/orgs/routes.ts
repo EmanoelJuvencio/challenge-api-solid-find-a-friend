62b82a3789eee484a8cc6eced012cb3bdfe576ca
Emanoel <emanoeljuvencio@gmail.com>
2025-01-01 13:05:38 -0300

    feat: Implementando refresh token

--- src/http/controller/orgs/routes.ts
+++ src/http/controller/orgs/routes.ts
@@ -1,11 +1,14 @@
 import { FastifyInstance } from 'fastify'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+
 import { register } from './register'
 import { authenticate } from './authenticate'
-import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post('/orgs', register)
   app.post('/authenticate', authenticate)
+  app.get('/refresh-token', refresh)
 
   // Daqui para baixo Rotas authenticadas
   app.addHook('onRequest', verifyJWT)
